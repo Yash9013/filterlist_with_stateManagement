10500c9a3d268d7f6cf791c3fcbf79110eb13fd0
Yash Katariya <yashkatariya2000@gmail.com>
2023-08-10 21:02:58 +0530

    Added Company Screen

--- lib/screen/crypto_screen.dart
+++ lib/screen/crypto_screen.dart
@@ -1,4 +1,5 @@
 import 'package:filterlistbloc/getxcontroller/crypto_controller.dart';
+import 'package:filterlistbloc/screen/company_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -48,6 +49,12 @@ class _CryptoScreenState extends State<CryptoScreen> {
               ),
             );
           },
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => const CompanyScreen()),
+          ),
+          child: const Icon(Icons.arrow_forward),
         ),
       ),
     );
